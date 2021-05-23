29131040e2aadc034f85cebfe2ad558e21365bc1
github username <github email address>
2021-05-23 19:51:06 -0400

    practice fetch request with the useEffect hook in React

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import { useEffect, useState } from "react";
+const App = () => {
+  const [isLoaded, setisLoaded] = useState(false);
+  const [dogImg, setdogImg] = useState("");
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setisLoaded(true);
+        setdogImg(data.message);
+      });
+  }, []);
+
+  return (
+    <div>
+      {isLoaded ? (
+        <img src={dogImg} alt="A Random Dog"></img>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default App;
